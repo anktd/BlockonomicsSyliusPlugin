9fd5a096782f91c2ef19b586a00652c4fa585f13
anktd <ankit61d@gmail.com>
2024-07-18 17:50:06 +0530

    implement basic plugin functionality

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blockonomics\SyliusPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('blockonomics_sylius_plugin');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->scalarNode('api_key')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Payum/BlockonomicsGatewayFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blockonomics\SyliusPlugin\Payum;
+
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayFactory;
+
+final class BlockonomicsGatewayFactory extends GatewayFactory
+{
+    protected function populateConfig(ArrayObject $config): void
+    {
+        $config->defaults([
+            'payum.factory_name' => 'blockonomics',
+            'payum.factory_title' => 'Blockonomics',
+        ]);
+    }
+}
